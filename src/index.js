3bb90546136d13c0ae922ca92a538f66ab673c7e
Manish Devrani <manishdevrani777@gmail.com>
2022-04-17 16:25:11 +0530

    feat-added quiz mock backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { makeServer } from './server';
+
+// Call make Server
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +13,5 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
