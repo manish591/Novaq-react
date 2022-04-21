31257cdec798bc46218567d2bbf30e6d66b6cbbd
Manish Devrani <manishdevrani777@gmail.com>
2022-04-22 00:04:58 +0530

    feat-added firebase store config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyChtlkduXmo0XbrofVuHAsMDVQzM3hKumk',
@@ -15,4 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export { app, analytics };
+// Initialize firestore
+const db = getFirestore();
+
+export { app, analytics, db };
