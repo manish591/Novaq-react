ab1bda288faaa4e20c11079eef9abf091661b0e0
Manish Devrani <manishdevrani777@gmail.com>
2022-04-21 16:12:11 +0530

    feat-modified react router routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import { CategoryPage, LandingPage, QuestionPage, Result, Rules } from 'pages';
+import {
+  CategoryPage,
+  LandingPage,
+  QuestionPage,
+  Result,
+  Rules,
+  QuizContainer
+} from 'pages';
 
 const App = () => (
   <div className="App">
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/rules" element={<Rules />} />
-      <Route path="/result" element={<Result />} />
+      <Route path="/quiz/:quizId" element={<QuizContainer />}>
+        <Route index element={<Rules />} />
+        <Route path="result" element={<Result />} />
+        <Route path="question" element={<QuestionPage />} />
+      </Route>
       <Route path="/category" element={<CategoryPage />} />
-      <Route path="/question" element={<QuestionPage />} />
     </Routes>
   </div>
 );
