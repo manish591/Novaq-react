1ffd2a4ba6d81e375a7d27a10c3c5e60c097a768
Manish Devrani <manishdevrani777@gmail.com>
2022-05-19 22:35:29 +0530

    style-added profile page design

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import './Profile.css';
 import { useAuthService, useScrollToTop } from 'hooks';
+import { Navbar } from 'components';
 
 const Profile = () => {
   const { logout } = useAuthService();
   useScrollToTop();
   return (
-    <div className="profile">
-      <h1>Name: Manish</h1>
-      <h1>Email: manishdevrani777@gmail.com</h1>
-      <button
-        type="button"
-        className="btn btn--contained-primary"
-        onClick={() => {
-          logout();
-        }}>
-        Logout
-      </button>
-    </div>
+    <>
+      <header className="header grid">
+        <Navbar />
+      </header>
+      <div className="profile">
+        <div className="profile__wrapper">
+          <section className="profile__content">
+            <div className="profile__details flex">
+              <p>Name:</p>
+              <h1>Manish</h1>
+            </div>
+            <div className="profile__details flex">
+              <p>Email:</p>
+              <h1>manishdevrani777@gmail.com</h1>
+            </div>
+            <button
+              type="button"
+              className="btn btn--contained-primary profile__logout"
+              onClick={() => {
+                logout();
+              }}>
+              Logout
+            </button>
+          </section>
+        </div>
+      </div>
+    </>
   );
 };
 
