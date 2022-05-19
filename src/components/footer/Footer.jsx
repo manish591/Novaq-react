0c84cf85f214d8e9290a39c2563ee17dad320199
Manish Devrani <manishdevrani777@gmail.com>
2022-05-20 03:12:17 +0530

    added footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer className="footer">
     <div className="footer__wrapper grid">
       <ul className="footer__list">
         <li className="footer__item">
-          <h2 className="footer__logo">NovaQ</h2>
+          <h2 className="footer__logo">
+            <Link to="/">NovaQ</Link>
+          </h2>
         </li>
       </ul>
       <ul className="footer__list footer__list--links flex">
-        <li className="footer__item">Home</li>
-        <li className="footer__item">Dashboard</li>
-        <li className="footer__item">Profile</li>
+        <li className="footer__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="footer__item">
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li className="footer__item">
+          <Link to="/profile">Profile</Link>
+        </li>
       </ul>
       <ul className="footer__list footer__list--socials flex">
-        <li className="footer__item">Twitter</li>
-        <li className="footer__item">LinkedIn</li>
-        <li className="footer__item">Github</li>
+        <li className="footer__item">
+          <a href="https://twitter.com/manishdevrani77">Twitter</a>
+        </li>
+        <li className="footer__item">
+          <a href="https://www.linkedin.com/in/manishdevrani77/">LinkedIn</a>
+        </li>
+        <li className="footer__item">
+          <a href="https://github.com/manish591">Github</a>
+        </li>
       </ul>
     </div>
   </footer>
