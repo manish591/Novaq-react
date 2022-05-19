540ec08357698adaded1d16a4a8907b5cd429e3b
Manish Devrani <manishdevrani777@gmail.com>
2022-05-20 02:06:16 +0530

    added user leaderboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 import { Navbar } from 'components';
+import { db } from 'firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Dashboard = () => {
+  const [leaderboardData, setLeaderboardData] = useState([]);
+  const usersDataRef = collection(db, 'users');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getDocs(usersDataRef);
+        const users = res.docs.map((item) => ({ ...item.data() }));
+        const sortedUsers = [...users].sort((a, b) => b.score - a.score);
+        setLeaderboardData(sortedUsers);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <header className="header grid">
@@ -10,7 +28,19 @@ const Dashboard = () => {
       </header>
       <div className="dashboard">
         <div className="dashboard__wrapper">
-          <h1>Dashboard</h1>
+          <section>
+            <h1 className="dashboard__title">Leaderboard</h1>
+          </section>
+          <section className="leaderboard">
+            {leaderboardData.map((item) => {
+              return (
+                <div key={item.userID} className="leaderboard__item">
+                  <h3>User: {item.email}</h3>
+                  <h3>Score: {item.score}</h3>
+                </div>
+              );
+            })}
+          </section>
         </div>
       </div>
     </>
