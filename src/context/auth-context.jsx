d985c26c5a85ee33dd44e3fe15289a4831fd6fd8
Manish Devrani <manishdevrani777@gmail.com>
2022-05-19 23:54:37 +0530

    added user profile details in profile page

--- src/context/auth-context.jsx
+++ src/context/auth-context.jsx
@@ -4,16 +4,23 @@ import PropTypes from 'prop-types';
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-  const [token, setToken] = useState('');
-  const [userID, setUserID] = useState('');
+  const [token, setToken] = useState(JSON.parse(localStorage.getItem('token')));
+  const [userID, setUserID] = useState(
+    JSON.parse(localStorage.getItem('userID'))
+  );
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem('user'))
+  );
   return (
     <AuthContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         token,
         userID,
+        currentUser,
         setToken,
-        setUserID
+        setUserID,
+        setCurrentUser
       }}>
       {children}
     </AuthContext.Provider>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Profile.css';
-import { useAuthService, useScrollToTop } from 'hooks';
+import { useAuthService, useScrollToTop, useAuthContext } from 'hooks';
 import { Navbar } from 'components';
 
 const Profile = () => {
   const { logout } = useAuthService();
+  const { currentUser } = useAuthContext();
   useScrollToTop();
   return (
     <>
@@ -16,11 +17,11 @@ const Profile = () => {
           <section className="profile__content">
             <div className="profile__details flex">
               <p>Name:</p>
-              <h1>Manish</h1>
+              <h1>{currentUser.split('@')[0]}</h1>
             </div>
             <div className="profile__details flex">
               <p>Email:</p>
-              <h1>manishdevrani777@gmail.com</h1>
+              <h1>{currentUser}</h1>
             </div>
             <button
               type="button"
