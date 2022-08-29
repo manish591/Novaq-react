27a33d601c67453c7b0579fc524a9fc3839e49a3
Manish Devrani <manishdevrani777@gmail.com>
2022-08-29 23:57:30 +0530

    style-added loading screeen

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -27,45 +27,49 @@ const CategoryPage = () => {
   useScrollToTop();
 
   return (
-    <div>
-      <header className="header grid">
-        <Navbar />
-      </header>
-      <main className="category-page grid">
-        <header className="category-page__header category-header">
-          <div className="category-header__wrapper">
-            <h2 className="category-header__title">
-              {categoryData?.categoryName}
-            </h2>
-            <p className="category-header__description">
-              {categoryData?.description}
-            </p>
-          </div>
-        </header>
-        <section className="category-page__list category-list">
-          {categoryData.quizzes && categoryData.quizzes.length < 1 ? (
-            <div className="category-page__coming-soon">
-              <h1>Coming Soon...</h1>
+    <div className="category-page-container">
+      <Navbar />
+      {Object.keys(categoryData).length ? (
+        <main className="category-page grid">
+          <header className="category-page__header category-header">
+            <div className="category-header__wrapper">
+              <h2 className="category-header__title">
+                {categoryData?.categoryName}
+              </h2>
+              <p className="category-header__description">
+                {categoryData?.description}
+              </p>
             </div>
-          ) : (
-            <div className="category-list__wrapper grid">
-              {categoryData.quizzes &&
-                categoryData?.quizzes.map((item) => {
-                  return (
-                    <QuizCard
-                      key={item._id}
-                      _id={item._id}
-                      totalQuestion={item.totalQuestion}
-                      categoryName={item.categoryName}
-                      title={item.title}
-                      image={item.image}
-                    />
-                  );
-                })}
-            </div>
-          )}
-        </section>
-      </main>
+          </header>
+          <section className="category-page__list category-list">
+            {categoryData.quizzes && categoryData.quizzes.length < 1 ? (
+              <div className="category-page__coming-soon">
+                <h1>Coming Soon...</h1>
+              </div>
+            ) : (
+              <div className="category-list__wrapper grid">
+                {categoryData.quizzes &&
+                  categoryData?.quizzes.map((item) => {
+                    return (
+                      <QuizCard
+                        key={item._id}
+                        _id={item._id}
+                        totalQuestion={item.totalQuestion}
+                        categoryName={item.categoryName}
+                        title={item.title}
+                        image={item.image}
+                      />
+                    );
+                  })}
+              </div>
+            )}
+          </section>
+        </main>
+      ) : (
+        <div className="category-page__empty">
+          <h1>Loading..</h1>
+        </div>
+      )}
       <Footer />
     </div>
   );
